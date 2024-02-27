8c995da7220eb39770ee126a127443bd8b61a170
SergeyZakurakin <117861728+SergeyZakurakin@users.noreply.github.com>
2024-02-27 19:57:55 +0000

    Initial Commit

---
+++ TableView HM6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView HM6
+//
+//  Created by Sergey Zakurakin on 27/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
